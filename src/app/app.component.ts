e287a54a9facef332b0cf7e885a9a7e06c7c8af3
Silvio Giannini <s.giannini@tdnet.it>
2019-12-28 12:12:17 +0100

    gestione notifiche web push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { SwPush } from "@angular/service-worker";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'my-pwa';
+export class AppComponent implements OnInit {
+  title = "my-pwa";
+  notificationData: any = {};
+
+  constructor(private swPush: SwPush) {
+    if (this.swPush.isEnabled) {
+      this.swPush
+        .requestSubscription({
+          serverPublicKey:
+            "..."
+        })
+        .then(subscription => {
+          // send subscription to the server
+        })
+        .catch(console.error);
+    }
+  }
+
+  ngOnInit(): void {
+    this.swPush.notificationClicks.subscribe(data => {
+      console.log("Notification click: ", data);
+      this.notificationData = data;
+    });
+  }
 }
